8fff5ffc30ea3a17da7ee07064eac5d451bd45b6
davidalvarez93 <david.alfredo.alvarez.morales@uabc.edu.mx>
2019-05-24 12:29:21 -0700

    + FilterByTitle-Public-Private/Student-MyGroups/Student-Explore

--- src/app/student/student-mygroups/student-mygroups.component.ts
+++ src/app/student/student-mygroups/student-mygroups.component.ts
@@ -9,21 +9,67 @@ import { Group } from 'src/app/models/group';
   styleUrls: ['./student-mygroups.component.css']
 })
 export class StudentMygroupsComponent implements OnInit {
+  selectedValue: string;
+  searchText: string="";
 
   groups = [];
+  groupsFilter = [];
 
   constructor(private endpoint : EndpointsService) { }
 
   ngOnInit() {
+    var elems = document.querySelectorAll('select');
+    M.FormSelect.init(elems);
     this.endpoint.get_StudentRegisteredGroups().
     subscribe(
       data => {
         this.groups = data;
+        this.groupsFilter = this.groups;
+        console.log(this.groups);
       },
       error =>{
         console.log("Error", error);
       }
     )
+  }
+
+  Search(){
+    if(this.searchText != ""){
+      this.groups = this.groups.filter(res=>{
+        return res.title.toLocaleLowerCase().match(this.searchText.toLocaleLowerCase())
+        || res.owner.firstName.toLocaleLowerCase().match(this.searchText.toLocaleLowerCase())
+        || res.owner.email.toLocaleLowerCase().match(this.searchText.toLocaleLowerCase());
+      });
+    }
+    else if(this.searchText == ""){
+      this.onChange();
+    }
+  }
+
+  onChange(){
+    console.log(this.selectedValue);
+    switch(this.selectedValue){
+      case '1':{
+        this.ngOnInit();
+        break;
+      }
+      case '2':{
+        this.groups = this.groupsFilter;
+        this.groups = this.groups.filter(element => {
+        return element.publicGroup === true;
+        });
+        break;
+      }
+      case '3':{
+        this.groups = this.groupsFilter;
+        this.groups = this.groups.filter(element => {
+        return element.publicGroup === false;
+        });
+        break;
+      }
+      
+    }
+
   }
 
 }
