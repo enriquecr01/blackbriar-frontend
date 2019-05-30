aabf1da178cc98a2167f425d778dba1865630628
davidalvarez93 <david.alfredo.alvarez.morales@uabc.edu.mx>
2019-05-30 10:07:17 -0700

    +Lista de alumnos

--- src/app/instructor/instructor-group/instructor-group.component.ts
+++ src/app/instructor/instructor-group/instructor-group.component.ts
@@ -12,10 +12,8 @@ export class InstructorGroupComponent implements OnInit {
 
   ngOnInit() {
   
-    $(document).ready(function(){
-      $('.fixed-action-btn').floatingActionButton();
-    });
-   
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
   }
 
 }
