844e52df97ce65431e6f7c043327a5a3815020c9
davidalvarez93 <david.alfredo.alvarez.morales@uabc.edu.mx>
2019-05-30 11:22:35 -0700

    merge dev -> dalvarez

--- src/app/instructor/instructor-group/instructor-group.component.ts
+++ src/app/instructor/instructor-group/instructor-group.component.ts
@@ -16,15 +16,10 @@ export class InstructorGroupComponent implements OnInit {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
   
-<<<<<<< HEAD
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems);
-=======
     // FLOATING BUTTON
     var elems = document.querySelectorAll('.fixed-action-btn');
     M.FloatingActionButton.init(elems);  
    
->>>>>>> dev
   }
 
 }
