c1324065d4e077e9e63dfb940875ae5271c56186
root <root@localhost.localdomain>
2019-06-07 10:07:20 -0700

    ForumInsert

--- src/app/instructor/instructor-group/instructor-group.component.ts
+++ src/app/instructor/instructor-group/instructor-group.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ForumInsertService } from 'src/app/services/forum-insert.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Forum } from 'src/app/models/forum';
 
 
 @Component({
@@ -7,12 +10,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./instructor-group.component.css']
 })
 export class InstructorGroupComponent implements OnInit {
-
+    
+  forumTitle: string;
+  description: string;
+  endDate: string;
+  warriorScore: number;
+  healerScore: number;
+  warlockScore: number;
+  answerScore: number;
+  published: boolean;
 
   @Input()
   groupId: number;
+  ForumInsertService: ForumInsertService;
 
-  constructor() { }
+  constructor(private forumInsertService: ForumInsertService) { }
 
   ngOnInit() {
     
@@ -24,13 +36,50 @@ export class InstructorGroupComponent implements OnInit {
     M.Datepicker.init(elems);
     // MODAL - TIME PICKER START
     var elems = document.querySelectorAll('.timepicker');
-    M.Timepicker.init(elems);
-
-  
+    M.Timepicker.init(elems);  
     // FLOATING BUTTON
     var elems = document.querySelectorAll('.fixed-action-btn');
     M.FloatingActionButton.init(elems);  
    
+  }
+
+  InsertForum(){
+    console.log("LE FUUUUCKIIIIING DATA");
+      console.log( "Title = " + this.forumTitle);
+        console.log( "Description = " + this.description);
+          console.log( "End Date = " + this.endDate);
+            console.log( "Warrior Score = " + this.warriorScore);
+              console.log( "Healer Score = " + this.healerScore);
+                console.log( "Warlcok Score = " + this.warlockScore);
+                  console.log( "Answer Score = " + this.answerScore);
+
+          var forum = new Forum();
+          forum.title = this.forumTitle,
+          forum.description = this.description,
+          forum.content = "content"
+          forum.endDate = "2019-06-04T05:35:37.659Z",
+          forum.warriorPoints = this.warriorScore,
+          forum.healerPoints = this.healerScore,
+          forum.warlockPoints = this.warlockScore,
+          forum.validResponsePoints = this.answerScore,
+          forum.published = this.published
+
+          this.forumInsertService.addForum(forum).
+          subscribe(
+            data  => 
+            { 
+                },
+                error => { 
+                  M.toast(error.error.message); 
+                  console.log(error.error.message);
+                }
+              );
+        error  => 
+        { 
+          console.log(error.error.message);
+          
+        }
+      
   }
 
 }
---
+++ src/app/models/forum.ts
@@ -0,0 +1,23 @@
+import { Timestamp } from 'rxjs';
+
+export class Forum {
+
+    //GroupId: number;
+    
+    title: string;
+    description: string;
+    content: string;
+    //startDate: string;
+    endDate: string;
+    warriorPoints: number;
+    healerPoints: number;
+    warlockPoints: number;
+    validResponsePoints: number;
+    published: boolean;
+
+   /*
+    "2019-06-04T05:35:37.659Z",
+    "2019-07-05T05:35:37.659Z",
+    */
+
+}
