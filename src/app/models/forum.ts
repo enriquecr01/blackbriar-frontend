9441b50f38ef2f05d96590740244e440d1118b43
Enrique Chavez R <enriquecr01@live.com>
2019-06-07 13:03:14 -0700

    errores arreglados

--- src/app/models/forum.ts
+++ src/app/models/forum.ts
@@ -34,7 +34,6 @@ export interface Forum {
     eDate: string;
     eTime: string;
     smallDescription: string;
-    published: boolean;
     warriorPoints: number;
     healerPoints: number;
     warlockPoints: number;
