8c1ffe765a08b6adc3a738c4aab3c1bbf802efee
Enrique Chavez R <enriquecr01@live.com>
2019-06-20 11:01:10 -0700

    No se xd

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -7,26 +7,21 @@ import { Answer } from 'src/app/models/answer';
   styleUrls: ['./comment.component.css']
 })
 export class CommentComponent implements OnInit, AfterViewInit {
-  ngAfterViewInit(): void {
-    var coll = document.getElementsByClassName("collapsible-comment");
+  ngAfterViewInit() {
+    var coll = document.getElementsByClassName("collapsible-comment ");
     var i;
-    
-    for (i = 0; i < coll.length; i++) 
-    {
-      coll[i].addEventListener("click", function() 
-      {
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if (content.style.maxHeight)
-        {
+        console.log(this.nextElementSibling);
+        if (content.style.maxHeight){
           content.style.maxHeight = null;
-        } else 
-        {
+        } else {
           content.style.maxHeight = content.scrollHeight + "px";
         }
       });
     }
-    console.log(coll);
   }
 
   @Input() comment: Answer;
@@ -35,7 +30,24 @@ export class CommentComponent implements OnInit, AfterViewInit {
 
   ngOnInit() 
   {
-    console.log(this.comment);
+    //console.log(this.comment);
+  }
+
+    
+  toggleAccordian(event, index) {
+    var element = event.target;
+    element.classList.toggle("active");
+    if(this.comment[index].isActive) {
+      this.comment[index].isActive = false;
+    } else {
+      this.comment[index].isActive = true;
+    }      
+    var panel = element.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
   }
 
 }
